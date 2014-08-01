bb36fa685c467e1e9ed89d10b9f39b4926e9df99
Artem Prigoda <arteamon@gmail.com>
2014-08-01 21:19:23 +0400

    Scan class metadata before processing. It increases processing speed and make runtime checking more easy

---
+++ src/main/java/com/github/arteam/json/rpc/simple/server/metadata/ParameterMetadata.java
@@ -0,0 +1,60 @@
+package com.github.arteam.json.rpc.simple.server.metadata;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Date: 8/1/14
+ * Time: 7:44 PM
+ * Method parameter metadata
+ *
+ * @author Artem Prigoda
+ */
+public class ParameterMetadata {
+
+    /**
+     * RPC name
+     */
+    @NotNull
+    private final String name;
+
+    /**
+     * Actual java type
+     */
+    @NotNull
+    private final Class<?> type;
+
+    /**
+     * Index in method arguments
+     */
+    private final int index;
+
+    /**
+     * Whether parameter is optional
+     */
+    private final boolean optional;
+
+    public ParameterMetadata(@NotNull String name, @NotNull Class<?> type, int index, boolean optional) {
+        this.name = name;
+        this.type = type;
+        this.index = index;
+        this.optional = optional;
+    }
+
+    @NotNull
+    public String getName() {
+        return name;
+    }
+
+    @NotNull
+    public Class<?> getType() {
+        return type;
+    }
+
+    public boolean isOptional() {
+        return optional;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
