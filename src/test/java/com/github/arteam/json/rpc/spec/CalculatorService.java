8ab1aa72451213b7fd1d36998ff4c4b3a345e4b4
Artem Prigoda <arteamon@gmail.com>
2014-08-01 13:00:56 +0400

    Test cases of JSON-RPC 2.0 spec http://www.jsonrpc.org/specification

---
+++ src/test/java/com/github/arteam/json/rpc/spec/CalculatorService.java
@@ -0,0 +1,36 @@
+package com.github.arteam.json.rpc.spec;
+
+import com.github.arteam.json.rpc.simple.annotation.JsonRpcMethod;
+import com.github.arteam.json.rpc.simple.annotation.JsonRpcParam;
+
+/**
+ * Date: 7/31/14
+ * Time: 8:42 PM
+ *
+ * @author Artem Prigoda
+ */
+public class CalculatorService {
+
+    @JsonRpcMethod
+    public long subtract(@JsonRpcParam("minuend") int m, @JsonRpcParam("subtrahend") int s) {
+        return (long) m - (long) s;
+    }
+
+    @JsonRpcMethod
+    public long sum(@JsonRpcParam("first") int first, @JsonRpcParam("second") int second,
+                    @JsonRpcParam("third") int third) {
+        return (long) first + (long) second + (long) third;
+    }
+
+    @JsonRpcMethod
+    public void update(@JsonRpcParam("i1") int i1, @JsonRpcParam("i2") int i2,
+                       @JsonRpcParam("i3") int i3, @JsonRpcParam("i4") int i4,
+                       @JsonRpcParam("i5") int i5) {
+        System.out.println("Update");
+    }
+
+    @JsonRpcMethod("get_data")
+    public Object[] getData() {
+        return new Object[]{"hello", 5};
+    }
+}
