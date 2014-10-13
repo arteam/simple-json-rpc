3602a60ffed4c10b71d7dc052e4c218fe78671f1
Artem Prigoda <arteamon@gmail.com>
2014-10-13 23:27:59 +0400

    More tests on batch processing

--- client/src/test/java/com/github/arteam/simplejsonrpc/client/BatchRequestBuilderTest.java
+++ client/src/test/java/com/github/arteam/simplejsonrpc/client/BatchRequestBuilderTest.java
@@ -1,19 +1,19 @@
 package com.github.arteam.simplejsonrpc.client;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.type.SimpleType;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.github.arteam.simplejsonrpc.client.domain.Player;
 import com.google.common.collect.ImmutableMap;
-import org.assertj.core.api.Assertions;
 import org.jetbrains.annotations.NotNull;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -57,7 +57,7 @@ public class BatchRequestBuilderTest {
     }
 
     @Test
-    public void test() {
+    public void testBatchCommonType() {
         JsonRpcClient client = initClient("batch");
         Map<String, Player> result = client.createBatchRequest()
                 .add("43121", "findByInitials", ImmutableMap.of("firstName", "Steven", "lastName", "Stamkos"))
@@ -66,10 +66,85 @@ public class BatchRequestBuilderTest {
                 .keysType(String.class)
                 .valuesType(Player.class)
                 .execute();
-        Assertions.assertThat(result.get("43121").getFirstName()).isEqualTo("Steven");
-        Assertions.assertThat(result.get("43121").getLastName()).isEqualTo("Stamkos");
-        Assertions.assertThat(result.get("43122").getFirstName()).isEqualTo("Jack");
-        Assertions.assertThat(result.get("43122").getLastName()).isEqualTo("Allen");
-        Assertions.assertThat(result.get("43123")).isNull();
+        assertThat(result.get("43121").getFirstName()).isEqualTo("Steven");
+        assertThat(result.get("43121").getLastName()).isEqualTo("Stamkos");
+        assertThat(result.get("43122").getFirstName()).isEqualTo("Jack");
+        assertThat(result.get("43122").getLastName()).isEqualTo("Allen");
+        assertThat(result.get("43123")).isNull();
+    }
+
+    @Test
+    public void testBatchDetailedTypes() {
+        JsonRpcClient client = initClient("batch");
+        Map<String, ?> result = client.createBatchRequest()
+                .add("43121", "findByInitials", ImmutableMap.of("firstName", "Steven", "lastName", "Stamkos"), Player.class)
+                .add("43122", "findByInitials", ImmutableMap.of("firstName", "Jack", "lastName", "Allen"), Player.class)
+                .add("43123", "findByInitials", ImmutableMap.of("firstName", "Vladimir", "lastName", "Sobotka"), Player.class)
+                .keysType(String.class)
+                .execute();
+        assertThat(result.get("43121")).isExactlyInstanceOf(Player.class);
+        assertThat(result.get("43122")).isExactlyInstanceOf(Player.class);
+        assertThat(((Player) result.get("43121")).getFirstName()).isEqualTo("Steven");
+        assertThat(((Player) result.get("43121")).getLastName()).isEqualTo("Stamkos");
+        assertThat(((Player) result.get("43122")).getFirstName()).isEqualTo("Jack");
+        assertThat(((Player) result.get("43122")).getLastName()).isEqualTo("Allen");
+        assertThat(result.get("43123")).isNull();
+    }
+
+    @Test
+    public void testBatchArrayRequests() {
+        JsonRpcClient client = initClient("batch_array");
+        Map<String, Player> result = client.createBatchRequest()
+                .add("43121", "findByInitials", "Steven", "Stamkos")
+                .add("43122", "findByInitials", "Jack", "Allen")
+                .add("43123", "findByInitials", "Vladimir", "Sobotka")
+                .keysType(String.class)
+                .valuesType(Player.class)
+                .execute();
+        assertThat(result.get("43121").getFirstName()).isEqualTo("Steven");
+        assertThat(result.get("43121").getLastName()).isEqualTo("Stamkos");
+        assertThat(result.get("43122").getFirstName()).isEqualTo("Jack");
+        assertThat(result.get("43122").getLastName()).isEqualTo("Allen");
+        assertThat(result.get("43123")).isNull();
+    }
+
+    @Test
+    public void testBatchArrayRequestsWithDetailedTypes() {
+        JsonRpcClient client = initClient("batch_array");
+        Map<String, ?> result = client.createBatchRequest()
+                .add("43121", "findByInitials", new Object[]{"Steven", "Stamkos"}, Player.class)
+                .add("43122", "findByInitials", new Object[]{"Jack", "Allen"}, Player.class)
+                .add("43123", "findByInitials", new Object[]{"Vladimir", "Sobotka"}, Player.class)
+                .keysType(String.class)
+                .execute();
+        assertThat(result.get("43121")).isExactlyInstanceOf(Player.class);
+        assertThat(result.get("43122")).isExactlyInstanceOf(Player.class);
+        assertThat(((Player) result.get("43121")).getFirstName()).isEqualTo("Steven");
+        assertThat(((Player) result.get("43121")).getLastName()).isEqualTo("Stamkos");
+        assertThat(((Player) result.get("43122")).getFirstName()).isEqualTo("Jack");
+        assertThat(((Player) result.get("43122")).getLastName()).isEqualTo("Allen");
+        assertThat(result.get("43123")).isNull();
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testDifferentRequests() {
+        JsonRpcClient client = initClient("different_requests");
+        Map<Integer, ?> result = client.createBatchRequest()
+                .add(12000, "isAlive", new HashMap<String, Object>(), Boolean.class)
+                .add(12001, "findByInitials", "Kevin", "Shattenkirk").returnType(12001, Player.class)
+                .add(12002, "find_by_birth_year", ImmutableMap.of("birth_year", 1990))
+                .returnType(12002, new TypeReference<List<Player>>() {
+                })
+                .keysType(Integer.class)
+                .execute();
+        assertThat(result.get(12000)).isExactlyInstanceOf(Boolean.class);
+        assertThat((Boolean) result.get(12000)).isTrue();
+        assertThat(result.get(12001)).isExactlyInstanceOf(Player.class);
+        assertThat(((Player) result.get(12001)).getFirstName()).isEqualTo("Kevin");
+        assertThat(((Player) result.get(12001)).getLastName()).isEqualTo("Shattenkirk");
+        assertThat(result.get(12002)).isInstanceOf(List.class);
+        assertThat((List<Player>) result.get(12002)).hasSize(3);
+
     }
 }
