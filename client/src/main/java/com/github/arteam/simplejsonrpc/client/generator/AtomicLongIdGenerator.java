6fe33f893ead5a2b3acb0ab760dd9eddff2f32f2
Artem Prigoda <arteamon@gmail.com>
2014-12-30 23:44:44 +0300

    Add AtomicLongIdGenerator
    It's more reliable and straightforward than CurrentTimeIdGenerator

---
+++ client/src/main/java/com/github/arteam/simplejsonrpc/client/generator/AtomicLongIdGenerator.java
@@ -0,0 +1,22 @@
+package com.github.arteam.simplejsonrpc.client.generator;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Date: 12/30/14
+ * Time: 11:19 PM
+ * <p/>
+ * Return id from an atomic long counter
+ * It's the most reliable and straightforward way to generate identifiers
+ *
+ * @author Artem Prigoda
+ */
+public class AtomicLongIdGenerator implements IdGenerator<Long> {
+
+    private AtomicLong id = new AtomicLong(0L);
+
+    @Override
+    public Long generate() {
+        return id.incrementAndGet();
+    }
+}
