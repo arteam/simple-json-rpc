0a555ede87cc73a2c640a6b053babe73321d5d1b
Artem Prigoda <arteamon@gmail.com>
2015-01-12 23:34:29 +0300

    Add SecureRandomLongIdGenerator
    It generates secure random positive longs.
    It's useful if you like to work with numbers rather
    then whith strings.
    
    It should also has to have better performance.

---
+++ client/src/main/java/com/github/arteam/simplejsonrpc/client/generator/SecureRandomIdGenerator.java
@@ -0,0 +1,36 @@
+package com.github.arteam.simplejsonrpc.client.generator;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+/**
+ * Date: 1/12/15
+ * Time: 11:17 PM
+ * Abstract generator of secure random identifiers
+ *
+ * @author Artem Prigoda
+ */
+public abstract class SecureRandomIdGenerator<T> implements IdGenerator<T> {
+
+    private static final String SHA_1_PRNG = "SHA1PRNG";
+
+    @NotNull
+    protected final SecureRandom secureRandom;
+
+    protected SecureRandomIdGenerator() {
+        secureRandom = initSecureRandom();
+    }
+
+    @NotNull
+    private static SecureRandom initSecureRandom() {
+        try {
+            SecureRandom secureRandom = SecureRandom.getInstance(SHA_1_PRNG);
+            secureRandom.nextBytes(new byte[]{}); // Important to seed immediately after creation
+            return secureRandom;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
