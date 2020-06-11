e21d5192c779d98730f5da3aa49eac93a63fa304
Artem Prigoda <arteamon@gmail.com>
2020-06-11 23:17:53 +0200

    Make `ErrorMessage.data` nullable
    It can actually be null

--- core/src/main/java/com/github/arteam/simplejsonrpc/core/domain/ErrorMessage.java
+++ core/src/main/java/com/github/arteam/simplejsonrpc/core/domain/ErrorMessage.java
@@ -1,11 +1,9 @@
 package com.github.arteam.simplejsonrpc.core.domain;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Date: 07.06.14
@@ -23,12 +21,12 @@ public class ErrorMessage {
     private final String message;
     
     @JsonProperty("data")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Nullable
     private final JsonNode data;
     
     public ErrorMessage(@JsonProperty("code") int code,
                         @JsonProperty("message") @NotNull String message,
-                        @JsonProperty("data") @NotNull JsonNode data) {
+                        @JsonProperty("data") @Nullable JsonNode data) {
         this.code = code;
         this.message = message;
         this.data = data;
@@ -42,7 +40,8 @@ public class ErrorMessage {
     public String getMessage() {
         return message;
     }
-    
+
+    @Nullable
     public JsonNode getData() {
         return data;
     }
