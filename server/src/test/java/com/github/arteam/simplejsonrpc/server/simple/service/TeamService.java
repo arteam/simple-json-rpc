967b5f5f6696875bdc6f81daac3aa2ac379f5dfc
Artem Prigoda <arteamon@gmail.com>
2020-07-18 20:21:32 +0200

    Use Streams API instead of Guava Collections

--- server/src/test/java/com/github/arteam/simplejsonrpc/server/simple/service/TeamService.java
+++ server/src/test/java/com/github/arteam/simplejsonrpc/server/simple/service/TeamService.java
@@ -24,6 +24,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Date: 7/27/14
@@ -34,16 +36,16 @@ public class TeamService extends BaseService {
 
     private static final DateTimeFormatter fmt = ISODateTimeFormat.date().withZone(DateTimeZone.UTC);
 
-    private List<Player> players = Lists.newArrayList(ImmutableList.<Player>builder()
-            .add(new Player("David", "Backes", new Team("St. Louis Blues", "NHL"), 42, Position.CENTER, date("1984-05-01"), 4.5))
-            .add(new Player("Vladimir", "Tarasenko", new Team("St. Louis Blues", "NHL"), 91, Position.RIGHT_WINGER, date("1991-12-13"), 0.9))
-            .add(new Player("Jack", "Allen", new Team("St. Louis Blues", "NHL"), 34, Position.GOALTENDER, date("1990-08-07"), 0.5))
-            .add(new Player("Jay", "Bouwmeester", new Team("St. Louis Blues", "NHL"), 19, Position.DEFENDER, date("1985-08-07"), 5.4))
-            .add(new Player("Steven", "Stamkos", new Team("Tampa Bay Lightning", "NHL"), 91, Position.CENTER, date("1990-02-07"), 7.5))
-            .add(new Player("Ryan", "Callahan", new Team("Tampa Bay Lightning", "NHL"), 24, Position.RIGHT_WINGER, date("1985-03-21"), 5.8))
-            .add(new Player("Ben", "Bishop", new Team("Tampa Bay Lightning", "NHL"), 30, Position.GOALTENDER, date("1986-11-21"), 2.3))
-            .add(new Player("Victor", "Hedman", new Team("Tampa Bay Lightning", "NHL"), 77, Position.DEFENDER, date("1990-12-18"), 4.0))
-            .build());
+    private final List<Player> players = Stream.of(
+            new Player("David", "Backes", new Team("St. Louis Blues", "NHL"), 42, Position.CENTER, date("1984-05-01"), 4.5),
+            new Player("Vladimir", "Tarasenko", new Team("St. Louis Blues", "NHL"), 91, Position.RIGHT_WINGER, date("1991-12-13"), 0.9),
+            new Player("Jack", "Allen", new Team("St. Louis Blues", "NHL"), 34, Position.GOALTENDER, date("1990-08-07"), 0.5),
+            new Player("Jay", "Bouwmeester", new Team("St. Louis Blues", "NHL"), 19, Position.DEFENDER, date("1985-08-07"), 5.4),
+            new Player("Steven", "Stamkos", new Team("Tampa Bay Lightning", "NHL"), 91, Position.CENTER, date("1990-02-07"), 7.5),
+            new Player("Ryan", "Callahan", new Team("Tampa Bay Lightning", "NHL"), 24, Position.RIGHT_WINGER, date("1985-03-21"), 5.8),
+            new Player("Ben", "Bishop", new Team("Tampa Bay Lightning", "NHL"), 30, Position.GOALTENDER, date("1986-11-21"), 2.3),
+            new Player("Victor", "Hedman", new Team("Tampa Bay Lightning", "NHL"), 77, Position.DEFENDER, date("1990-12-18"), 4.0))
+            .collect(Collectors.toList());
 
     @JsonRpcMethod
     public boolean add(@JsonRpcParam("player") Player s) {
@@ -52,48 +54,39 @@ public class TeamService extends BaseService {
 
     @JsonRpcMethod("find_by_birth_year")
     public List<Player> findByBirthYear(@JsonRpcParam("birth_year") final int birthYear) {
-        return ImmutableList.copyOf(Iterables.filter(players, new Predicate<Player>() {
-            @Override
-            public boolean apply(Player player) {
-                int year = new DateTime(player.getBirthDate()).getYear();
-                return year == birthYear;
-            }
-        }));
+        return players.stream().filter(player -> {
+            int year = new DateTime(player.getBirthDate()).getYear();
+            return year == birthYear;
+        }).collect(Collectors.toList());
     }
 
     @JsonRpcMethod
     public Player findByInitials(@JsonRpcParam("firstName") final String firstName,
                                  @JsonRpcParam("lastName") final String lastName) {
-        return Iterables.tryFind(players, new Predicate<Player>() {
-            @Override
-            public boolean apply(Player player) {
-                return player.getFirstName().equals(firstName) &&
-                        player.getLastName().equals(lastName);
-            }
-        }).orNull();
+        return players.stream()
+                .filter(player -> player.getFirstName().equals(firstName) && player.getLastName().equals(lastName))
+                .findAny()
+                .orElse(null);
     }
 
     @JsonRpcMethod
-    public List<Player> find(@JsonRpcOptional @JsonRpcParam("position") @Nullable final Position position,
-                             @JsonRpcOptional @JsonRpcParam("number") @Nullable final int number,
-                             @JsonRpcOptional @JsonRpcParam("team") @NotNull final Optional<Team> team,
-                             @JsonRpcOptional @JsonRpcParam("firstName") @Nullable final String firstName,
-                             @JsonRpcOptional @JsonRpcParam("lastName") @Nullable final String lastName,
-                             @JsonRpcOptional @JsonRpcParam("birthDate") @Nullable final Date birthDate,
-                             @JsonRpcOptional @JsonRpcParam("capHit") @NotNull final Optional<Double> capHit) {
-        return Lists.newArrayList(Iterables.filter(players, new Predicate<Player>() {
-            @Override
-            public boolean apply(Player player) {
-                if (position != null && !player.getPosition().equals(position)) return false;
-                if (number != 0 && player.getNumber() != number) return false;
-                if (team.isPresent() && !player.getTeam().equals(team.get())) return false;
-                if (firstName != null && !player.getFirstName().equals(firstName)) return false;
-                if (lastName != null && !player.getLastName().equals(lastName)) return false;
-                if (birthDate != null && !player.getBirthDate().equals(birthDate)) return false;
-                if (capHit.isPresent() && player.getCapHit() != capHit.get()) return false;
-                return true;
-            }
-        }));
+    public List<Player> find(@JsonRpcOptional @JsonRpcParam("position") @Nullable Position position,
+                             @JsonRpcOptional @JsonRpcParam("number") @Nullable int number,
+                             @JsonRpcOptional @JsonRpcParam("team") @NotNull Optional<Team> team,
+                             @JsonRpcOptional @JsonRpcParam("firstName") @Nullable String firstName,
+                             @JsonRpcOptional @JsonRpcParam("lastName") @Nullable String lastName,
+                             @JsonRpcOptional @JsonRpcParam("birthDate") @Nullable Date birthDate,
+                             @JsonRpcOptional @JsonRpcParam("capHit") @NotNull Optional<Double> capHit) {
+        return players.stream().filter(player -> {
+            if (position != null && !player.getPosition().equals(position)) return false;
+            if (number != 0 && player.getNumber() != number) return false;
+            if (team.isPresent() && !player.getTeam().equals(team.get())) return false;
+            if (firstName != null && !player.getFirstName().equals(firstName)) return false;
+            if (lastName != null && !player.getLastName().equals(lastName)) return false;
+            if (birthDate != null && !player.getBirthDate().equals(birthDate)) return false;
+            if (capHit.isPresent() && player.getCapHit() != capHit.get()) return false;
+            return true;
+        }).collect(Collectors.toList());
     }
 
     @JsonRpcMethod
@@ -207,42 +200,31 @@ public class TeamService extends BaseService {
 
     @JsonRpcMethod
     public List<Player> findPlayersByFirstNames(@JsonRpcParam("names") final List<String> names) {
-        return Lists.newArrayList(Iterables.filter(players, new Predicate<Player>() {
-            @Override
-            public boolean apply(Player player) {
-                return names.contains(player.getFirstName());
-            }
-        }));
+        return players.stream().filter(player -> names.contains(player.getFirstName())).collect(Collectors.toList());
     }
 
     @JsonRpcMethod
     public List<Player> findPlayersByNumbers(@JsonRpcParam("numbers") final int... numbers) {
-        return Lists.newArrayList(Iterables.filter(players, new Predicate<Player>() {
-            @Override
-            public boolean apply(Player player) {
-                for (int number : numbers) {
-                    if (player.getNumber() == number) {
-                        return true;
-                    }
+        return players.stream().filter(player -> {
+            for (int number : numbers) {
+                if (player.getNumber() == number) {
+                    return true;
                 }
-                return false;
             }
-        }));
+            return false;
+        }).collect(Collectors.toList());
     }
 
     @JsonRpcMethod
     public <T> List<Player> genericFindPlayersByNumbers(@JsonRpcParam("numbers") final T... numbers) {
-        return Lists.newArrayList(Iterables.filter(players, new Predicate<Player>() {
-            @Override
-            public boolean apply(Player player) {
-                for (T number : numbers) {
-                    if (String.valueOf(player.getNumber()).equals(number.toString())) {
-                        return true;
-                    }
+        return players.stream().filter(player -> {
+            for (T number : numbers) {
+                if (String.valueOf(player.getNumber()).equals(number.toString())) {
+                    return true;
                 }
-                return false;
             }
-        }));
+            return false;
+        }).collect(Collectors.toList());
     }
 
     @JsonRpcMethod
