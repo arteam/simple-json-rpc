282b31c1044201ff6d9042b56dc5be9edb9052c0
Artem Prigoda <arteamon@gmail.com>
2022-01-14 10:00:30 +0100

    Make IdGenerator a functional interface

--- client/src/main/java/com/github/arteam/simplejsonrpc/client/generator/IdGenerator.java
+++ client/src/main/java/com/github/arteam/simplejsonrpc/client/generator/IdGenerator.java
@@ -3,8 +3,10 @@ package com.github.arteam.simplejsonrpc.client.generator;
 /**
  * Date: 24.08.14
  * Time: 18:12
- * Strategy for generation request identificators
+ * <p>
+ * A strategy for the generation of request ids
  */
+@FunctionalInterface
 public interface IdGenerator<T> {
 
     T generate();
