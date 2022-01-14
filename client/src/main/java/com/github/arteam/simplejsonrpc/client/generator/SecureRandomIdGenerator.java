a48aa6d409885cc95cc806172e720122d30ca648
Artem Prigoda <arteamon@gmail.com>
2022-01-14 09:54:22 +0100

    Use Native secure random generator by default
    SHA1PRNG is a relic of the old Java 6 times.

--- client/src/main/java/com/github/arteam/simplejsonrpc/client/generator/SecureRandomIdGenerator.java
+++ client/src/main/java/com/github/arteam/simplejsonrpc/client/generator/SecureRandomIdGenerator.java
@@ -1,6 +1,5 @@
 package com.github.arteam.simplejsonrpc.client.generator;
 
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 /**
@@ -10,21 +9,13 @@ import java.security.SecureRandom;
  */
 abstract class SecureRandomIdGenerator<T> implements IdGenerator<T> {
 
-    private static final String SHA_1_PRNG = "SHA1PRNG";
-
     protected final SecureRandom secureRandom;
 
     protected SecureRandomIdGenerator() {
-        secureRandom = initSecureRandom();
+        this(new SecureRandom());
     }
 
-    private static SecureRandom initSecureRandom() {
-        try {
-            SecureRandom secureRandom = SecureRandom.getInstance(SHA_1_PRNG);
-            secureRandom.nextBytes(new byte[]{}); // Important to seed immediately after creation
-            return secureRandom;
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
+    protected SecureRandomIdGenerator(SecureRandom secureRandom) {
+        this.secureRandom = secureRandom;
     }
 }
